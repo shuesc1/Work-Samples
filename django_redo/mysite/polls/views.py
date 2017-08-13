2dccdde8a06177b2dde74a586b2f418c91490874
Joseph Haymaker <josepma@seas.upenn.edu>
2017-08-12 18:59:59 -0700

    continues intro to django

--- django_redo/mysite/polls/views.py
+++ django_redo/mysite/polls/views.py
@@ -1,9 +1,32 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.http import HttpResponse
-
-from django.shortcuts import render
+from django.http import Http404
+from django.template import loader
+from .models import Question
+from django.shortcuts import get_object_or_404, render
 
 def index(request):
-    return HttpResponse("Hello, world. You're at a polls index.")
+    # return HttpResponse("Hello, world. You're at a polls index.")
+    # latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    # output = ', '.join([q.question_text for q in latest_question_list])
+    # return HttpResponse(output)
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    # template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    # return HttpResponse(template.render(context, request))
+    return render(request, 'polls/index.html', context)
 
+def detail(request, question_id):
+    #return HttpResponse("You're looking at question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
