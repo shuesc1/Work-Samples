8822a3fca1e8309f709cfd818f937dfc9563f808
Joseph Haymaker <josephhaymaker@Josephs-MacBook-Pro.local>
2017-09-11 20:23:40 -0400

    updates the decryptor class

--- Encryption_CIS573_HW1/src/edu/upenn/cis573/hwk1/Decryptor.java
+++ Encryption_CIS573_HW1/src/edu/upenn/cis573/hwk1/Decryptor.java
@@ -2,19 +2,32 @@ package edu.upenn.cis573.hwk1;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.HashMap;
 import java.util.Scanner;
 
+/**
+ * A class that takes in a cipher list sorted by frequency and an original character list also sorted by frequency
+ * This class decrypts a given file using a frequency model
+ * @author josephhaymaker
+ *
+ */
 public class Decryptor {
 
 	private char[] baseCharsByFreq;
 	private char[] encryptionKeysByFreq;
 	private Scanner in;
+	private HashMap<Character, Character> encryptedCharWithKey = new HashMap<>();
 
 	public Decryptor(char[] baseCharsByFreq, char[] encryptionKeysByFreq) {
 		this.baseCharsByFreq = baseCharsByFreq;
 		this.encryptionKeysByFreq = encryptionKeysByFreq;
+		setEncryptedCharWithKey(createDecryptionKey());
 	}
 
+	/**
+	 * A method that decrypts a file and writes out the correctly decoded original content
+	 * @param encryptedFilename a valid name of a file encrypted using a substitution cipher
+	 */
 	public void decrypt(String encryptedFilename){
 		File file = new File(encryptedFilename) ;
 		try {
@@ -23,20 +36,60 @@ public class Decryptor {
 			System.out.println("File not found!") ;
 			e.printStackTrace();
 		}
+		//iterate over all lines in encrypted file
 		while(in.hasNextLine()) {
-			String currentEncryptedChar = in.next();
-			System.out.println(currentEncryptedChar) ;
-			if(baseCharsByFreq.toString().contains(currentEncryptedChar)) { 
-		    //if current char is part of the encrypted char set sorted by frequency (if it is in fact a letter)
+			//iterate over all tokens (characters)
+			while(in.hasNext()) {
+				String currentEncryptedChar = in.next();
+				System.out.println(currentEncryptedChar) ;
+				if(baseCharsByFreq.toString().contains(currentEncryptedChar)) { 
+					//if current char is part of the encrypted char set sorted by frequency (if it is in fact a letter)
 
-				//write out to file all chars swa
+					//write out to file all chars swa
 
+				}
 			}
 		}
+		in.close();
 	}
 
 
+	/**
+	 * A method that creates a HashMap with keys of ciphers and values of their corresponding original
+	 * characters based on frequency representations.
+	 * @return
+	 */
+	public HashMap<Character, Character> createDecryptionKey(){
+		HashMap<Character, Character> ciphersWithOrigChars = new HashMap<>();
+		for(int i = 0; i < baseCharsByFreq.length; i++) {
+			//add key of cipher with value of original correct character
+			//			if(encryptionKeysByFreq[i] != null && baseCharsByFreq[i] != null) { //trying to catch possible OOB error
+			ciphersWithOrigChars.put(encryptionKeysByFreq[i], baseCharsByFreq[i]) ;
+			//			}
+		}
+		return ciphersWithOrigChars;
+	}
 
+	/**
+	 * A getter method for the hashmap used to decode the text
+	 * @return
+	 */
+	public HashMap<Character, Character> getEncryptedCharWithKey() {
+		return encryptedCharWithKey;
+	}
+
+	/**
+	 * A setter method for the HashMap used to decode the text
+	 * @param encryptedCharWithKey
+	 */
+	public void setEncryptedCharWithKey(HashMap<Character, Character> encryptedCharWithKey) {
+		this.encryptedCharWithKey = encryptedCharWithKey;
+	}
+
+
+	/*=========================================
+	 * TESTING
+    ======================================== */
 	public static void main(String[] args) {
 
 		char[] baseSet = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'} ;
@@ -47,5 +100,7 @@ public class Decryptor {
 
 
 	}
+
+
 
 }
