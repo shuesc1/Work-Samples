a45badb412be2129141fe629732f4824cf272538
Joseph Haymaker <josephhaymaker@vagelos-ve507-0160.apn.wlan.upenn.edu>
2017-09-13 13:09:13 -0400

    continues adding exceptions in the Main method

--- Encryption_CIS573_HW1/src/edu/upenn/cis573/hwk1/Main.java
+++ Encryption_CIS573_HW1/src/edu/upenn/cis573/hwk1/Main.java
@@ -1,5 +1,8 @@
 package edu.upenn.cis573.hwk1;
 
+import java.io.File;
+import java.nio.file.InvalidPathException;
+import java.nio.file.NotDirectoryException;
 import java.text.DecimalFormat;
 
 public class Main {
@@ -14,32 +17,47 @@ public class Main {
 		String corpusDir, absoluteCorpusDir, relativeCorpusDir;
 
 		//=======OBTAIN RUNTIME ARGUMENT-ABSOLUTE OR RELATIVE PATH=========
-		//		boolean validPath = false;
-
-		//		while(validPath == false) {
-		// Accounts for: 1. the number of runtime arguments isn't correct
+		File argFilePath = new File(args[0]);
+		//CHECKS IF: 1. the number of runtime arguments isn't correct
 		if (args.length != 1) {
 			throw new IllegalArgumentException("Exactly 1 runtime argument ('{filepath}') required!");
 		}
 
-		corpusDir = args[0] ;
+		//CHECKS IF PATH IS DIRECTORY
+		if (argFilePath.isDirectory()) { //TODO does this check absolute and relative filepaths?
+			System.out.println("Filepath given is a directory.") ;
+		} else {
+			throw new IllegalArgumentException("Pathname specified is not a directory!") ;
+		}
+		
+		//CHECKS IF PATH EXISTS
+		if (argFilePath.exists()) {
+			System.out.println("The path indicated by the filepath does exist.") ;
+		} else if (!argFilePath.exists()) {
+			throw new InvalidPathException("Filepath does not exist!:", args[0]) ;
+		}
+		
+		//CHECKS IF PATH IS READABLE/WRITABLE
+		
+		
+		//TODO throw exceptions for 2. specified dir doesn't exist, 3. specified dir can't be opened for reading, 4. specified dir is empty
+		//InvalidPathException ? , DirectoryNotFoundException ?
+//		corpusDir = args[0] ;
 		
 //		try {
 //			corpusDir = args[0] ;
-//
-//		} catch() {
+//		} catch(DirectoryNotFoundException) {
 //			System.err.print("Please enter a *valid* runtime argument specifying an absolute or relative path to the target corpus;") ;
 //			System.err.println("Argument '" + args[0] + "' must be valid string.");
 //			System.exit(1);
-//		}
+
+
+
+		//========================END SECTION============================
+
 		
-
-		//		}
-
-
-
-		//=============================================================
-
+		
+		//========ITERATE OVER ALL FILES IN SPECIFIED DIRECTORY========
 		//
 		//get array of all files in target directory
 		//while there are more values in array of text files in directory
@@ -53,6 +71,9 @@ public class Main {
 		//6. store correctNum and incorrectNum for final tally
 		// }
 
+		//========================END SECTION============================
+		
+		//============CALCULATE ACCURACY AND PRINT OUT=================
 		if((totalCorrect + totalIncorrect) != 0) {
 			accuracy = totalCorrect / (totalCorrect + totalIncorrect) ;
 		} else {
