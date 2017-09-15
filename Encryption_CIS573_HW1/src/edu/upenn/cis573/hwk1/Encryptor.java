d0dec27cd3e7e04f46595aee40fc189260fb872c
Joseph Haymaker <josephhaymaker@Josephs-MacBook-Pro.local>
2017-09-14 22:06:57 -0400

    tries to stop the bleeding

--- Encryption_CIS573_HW1/src/edu/upenn/cis573/hwk1/Encryptor.java
+++ Encryption_CIS573_HW1/src/edu/upenn/cis573/hwk1/Encryptor.java
@@ -51,126 +51,139 @@ public class Encryptor {
 			System.out.println("File not found!") ;
 			e.printStackTrace();
 		}
-		while(in.hasNextLine()) {
-			String currentLine = "";
-			while(in.hasNext()) {
-				currentBaseChar = in.next().toLowerCase();
-				if(origCharsWithCiphers.containsKey(currentBaseChar)) { 	
-					currentBaseChar = origCharsWithCiphers.get(currentBaseChar) ;
-				}
-				currentLine = currentLine + currentBaseChar ;
-			}
-			currentLine = currentLine + "/n" ;
-			//TODO check if this is adding a newline correctly
-			//============GENERATED STRING OF 1 LINE OF ENCRYPTED FILE============
+		//		while(in.hasNextLine()) {
+		//			String currentLine = "";
+		//			while(in.hasNext()) {
+		//				currentBaseChar = in.next().toLowerCase();
+		//				if(origCharsWithCiphers.containsKey(currentBaseChar)) { 	
+		//					currentBaseChar = origCharsWithCiphers.get(currentBaseChar) ;
+		//				}
+		////				currentLine = currentLine + currentBaseChar ;
+		//			}
+		//			currentLine = currentLine + "/n" ;
+		//TODO check if this is adding a newline correctly
+		//============GENERATED STRING OF 1 LINE OF ENCRYPTED FILE============
 
-			//===========CREATE DECRYPTED FILE OR APPEND LINE TO EXISTING FILE=====================	
-			try {
-				encryptedFilename = nameEncryptedFile(origFilesName) ;
-				File encryptedFile = new File(filepath, nameEncryptedFile(origFilesName));
-				if (!encryptedFile.exists()) {
-					encryptedFile.createNewFile();
-				}
-				out = new PrintWriter(new BufferedWriter(new FileWriter(encryptedFile.getAbsoluteFile(), true)));
-				out.write(currentLine);
-				//				System.out.println("Done");
-			} catch (IOException e) {
-				e.printStackTrace();
-			} finally {
-				if (out != null)
-					out.close();
-				//==========CLOSE PRINTWRITER=====================
+		//===========CREATE DECRYPTED FILE OR APPEND LINE TO EXISTING FILE=====================	
+		try {
+			encryptedFilename = nameEncryptedFile(origFilesName) ;
+			File encryptedFile = new File(filepath, nameEncryptedFile(origFilesName));
+			if (!encryptedFile.exists()) {
+				encryptedFile.createNewFile();
 			}
+			out = new PrintWriter(new BufferedWriter(new FileWriter(encryptedFile.getAbsoluteFile(), true)));
+			while(in.hasNextLine()) {
+				String currentLine = "";
+				while(in.hasNext()) {
+					currentBaseChar = in.next().toLowerCase();
+					if(origCharsWithCiphers.containsKey(currentBaseChar)) { 	
+						currentBaseChar = origCharsWithCiphers.get(currentBaseChar) ;
+					}
+					//						currentLine = currentLine + currentBaseChar ;
+					out.write(currentBaseChar);
+				}
+			}
+
+
+			//				System.out.println("Done");
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (out != null)
+				out.close();
+			//==========CLOSE PRINTWRITER=====================
 		}
 		in.close();
 	}
+	
 
-	/**
-	 * A helper method that names the encrypted file that is to be written out
-	 * @param originalFilename 
-	 * @return decryptedFilename a string of the encrypted filename in the form (prefix)ENCRYPTED.(extension)
-	 */
-	public String nameEncryptedFile(String originalFilename) {
-		String prefix = "" ;
-		String extension = originalFilename.substring(originalFilename.lastIndexOf(".") + 1);
-		String encryptedFilename = "" ;
-		String namePattern = "[ \\w-]+?(?=\\.)";
-		Pattern p1 = Pattern.compile(namePattern);
-		Matcher m = p1.matcher(originalFilename);
-		if (m.find( )) {
-			prefix = m.group(0) ; 
-			
-		}else {
-//			System.out.println("No encrypted file match found!");
+
+/**
+ * A helper method that names the encrypted file that is to be written out
+ * @param originalFilename 
+ * @return decryptedFilename a string of the encrypted filename in the form (prefix)ENCRYPTED.(extension)
+ */
+public String nameEncryptedFile(String originalFilename) {
+	String prefix = "" ;
+	String extension = originalFilename.substring(originalFilename.lastIndexOf(".") + 1);
+	String encryptedFilename = "" ;
+	String namePattern = "[ \\w-]+?(?=\\.)";
+	Pattern p1 = Pattern.compile(namePattern);
+	Matcher m = p1.matcher(originalFilename);
+	if (m.find( )) {
+		prefix = m.group(0) ; 
+
+	}else {
+		//			System.out.println("No encrypted file match found!");
+	}
+
+
+	encryptedFilename = prefix + "ENCRYPTED." + extension ;
+
+	//		System.out.println("Prefix: " + prefix);
+	//		System.out.println("Extension: " + extension);
+	//		System.out.println(decryptedFilename);
+	return encryptedFilename;
+}
+
+/**
+ * A method that creates a HashMap with keys of original characters and values of their corresponding ciphers
+ * @return baseCharsWithCiphers a HashMap of chars mapped to ciphers
+ */
+public HashMap<String, String> createEncryptionKey(){
+	HashMap<String, String> baseCharsWithCiphers = new HashMap<>();
+	for(int i = 0; i < baseChars.length; i++) {
+		try {
+			baseCharsWithCiphers.put(baseChars[i], encryptionKeys[i]) ;
+		} catch(ArrayIndexOutOfBoundsException aioobe){
+			System.err.println("Error: number of base characters doesn't match number of cipher characters!");
 		}
-
-
-			encryptedFilename = prefix + "ENCRYPTED." + extension ;
-
-		//		System.out.println("Prefix: " + prefix);
-		//		System.out.println("Extension: " + extension);
-		//		System.out.println(decryptedFilename);
-		return encryptedFilename;
 	}
+	return baseCharsWithCiphers;
+}
 
-	/**
-	 * A method that creates a HashMap with keys of original characters and values of their corresponding ciphers
-	 * @return baseCharsWithCiphers a HashMap of chars mapped to ciphers
-	 */
-	public HashMap<String, String> createEncryptionKey(){
-		HashMap<String, String> baseCharsWithCiphers = new HashMap<>();
-		for(int i = 0; i < baseChars.length; i++) {
-			try {
-				baseCharsWithCiphers.put(baseChars[i], encryptionKeys[i]) ;
-			} catch(ArrayIndexOutOfBoundsException aioobe){
-				System.err.println("Error: number of base characters doesn't match number of cipher characters!");
-			}
-		}
-		return baseCharsWithCiphers;
-	}
+/**
+ * A getter method for the hashmap used to decode the text
+ * @return
+ */
+public HashMap<String, String> getCharWithCipher() {
+	return origCharsWithCiphers;
+}
 
-	/**
-	 * A getter method for the hashmap used to decode the text
-	 * @return
-	 */
-	public HashMap<String, String> getCharWithCipher() {
-		return origCharsWithCiphers;
-	}
+/**
+ * A setter method for the HashMap used to decode the text
+ * @param encryptedCharWithKey
+ */
+public void setCharWithCipher(HashMap<String, String> encryptedCharWithKey) {
+	this.origCharsWithCiphers = encryptedCharWithKey;
+}
 
-	/**
-	 * A setter method for the HashMap used to decode the text
-	 * @param encryptedCharWithKey
-	 */
-	public void setCharWithCipher(HashMap<String, String> encryptedCharWithKey) {
-		this.origCharsWithCiphers = encryptedCharWithKey;
-	}
+/**
+ * A getter method for the encrypted file's name
+ * @return
+ */
+public String getEncryptedFilename() {
+	return encryptedFilename;
+}
 
-	/**
-	 * A getter method for the encrypted file's name
-	 * @return
-	 */
-	public String getEncryptedFilename() {
-		return encryptedFilename;
-	}
+/**
+ * A setter method for the encrypted file's name
+ * @param decryptedFilename
+ */
+public void setEncryptedFilename(String decryptedFilename) {
+	this.encryptedFilename = decryptedFilename;
+}
 
-	/**
-	 * A setter method for the encrypted file's name
-	 * @param decryptedFilename
-	 */
-	public void setEncryptedFilename(String decryptedFilename) {
-		this.encryptedFilename = decryptedFilename;
+/**
+ * A helper method that assures that all characters are lowercase so they are sure to be matched
+ * @param keyChars
+ * @return 
+ */
+public String[] charsToLowercase(String[] keyChars) {
+	for (int i =0; i < keyChars.length; i++) {
+		keyChars[i] = keyChars[i].toLowerCase() ;
 	}
-
-	/**
-	 * A helper method that assures that all characters are lowercase so they are sure to be matched
-	 * @param keyChars
-	 * @return 
-	 */
-	public String[] charsToLowercase(String[] keyChars) {
-		for (int i =0; i < keyChars.length; i++) {
-			keyChars[i] = keyChars[i].toLowerCase() ;
-		}
-		return keyChars;
-	}
+	return keyChars;
+}
 
 }
