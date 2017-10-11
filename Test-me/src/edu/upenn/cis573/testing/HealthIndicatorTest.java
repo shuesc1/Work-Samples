1601844e39fca4f14913b257d2e2c08f92dd1148
Joseph Haymaker <josephhaymaker@vagelos-ve506-0186.apn.wlan.upenn.edu>
2017-10-11 13:23:38 -0400

    adds more test cases to HealthIndicator test class

--- Test-me/src/edu/upenn/cis573/testing/HealthIndicatorTest.java
+++ Test-me/src/edu/upenn/cis573/testing/HealthIndicatorTest.java
@@ -19,7 +19,7 @@ public class HealthIndicatorTest {
 
 	@Before 
 	public void initialize() {
-		hi = new HealthIndicator() ;
+//		hi = new HealthIndicator() ; //not needed since method is static
 	}
 
 	//=============================================================
@@ -38,44 +38,101 @@ public class HealthIndicatorTest {
 	//=============================================================
 	//age < 20 should return WeightStatus==invalid; age >= 20 should return categorical WeightStatus
 	@Test
-	public void testAgeBelow20Condition1() {
+	public void testAgeBelow20InvalidCondition1() {
 		int illegalAge = 19 ;
-		ws = hi.calculateWeightStatus(validHgt, validHgtIn, validWtPds, illegalAge) ;
+		ws = HealthIndicator.calculateWeightStatus(validHgt, validHgtIn, validWtPds, illegalAge) ;
 		assertEquals("an age below 20 should return a WeightStatus of 'invalid'", "INVALID", ws) ; 
 	}
 	
-	public void testAgeBelow20Condition2() {
+	public void testAgeBelow20InvalidCondition2() {
 		int illegalAge = 5 ;
-		ws = hi.calculateWeightStatus(validHgt, validHgtIn, validWtPds, illegalAge) ;
-		assertEquals("an age below 20 should return a WeightStatus of 'invalid'", "INVALID", ws) ; 
+		ws = HealthIndicator.calculateWeightStatus(validHgt, validHgtIn, validWtPds, illegalAge) ;
+		assertNotEquals("an age below 20 should return a WeightStatus of 'invalid'", "HEALTHY", ws) ; 
 	}
 	
 	@Test
-	public void testAgeBelow20Condition3() {
-		int legalAge = 20 ;
-		ws = hi.calculateWeightStatus(validHgt, validHgtIn, validWtPds, legalAge) ;
-		assertNotEquals("an age above 20 should return a WeightStatus other than 'invalid'", "INVALID", ws) ;
-	}
-	
-	@Test
-	public void testAgeBelow20Condition4() {
+	public void testAgeBelow20ValidCondition1() {
 		int legalAge = 30 ;
-		ws = hi.calculateWeightStatus(validHgt, validHgtIn, validWtPds, legalAge) ; //BMI==23.7 =(160.7*703)/(69^2)
+		ws = HealthIndicator.calculateWeightStatus(validHgt, validHgtIn, validWtPds, legalAge) ; //BMI==23.7 =(160.7*703)/(69^2)
 		assertEquals("an age above 20 should return a WeightStatus other than 'invalid'-- 23.7 == 'healthy' in this case", "HEALTHY", ws) ;
+	}
+	
+	@Test
+	public void testAgeBelow20ValidCondition2() {
+		int legalAge = 20 ;
+		ws = HealthIndicator.calculateWeightStatus(validHgt, validHgtIn, validWtPds, legalAge) ;
+		assertNotEquals("an age above 20 should return a WeightStatus other than 'invalid'", "INVALID", ws) ;
 	}
 	
 	//=============================================================
 	//================SPEC: Height Ft/Inch combo ==================
 	//=============================================================
 	//int height in feet should combine with double height in inches and output double height in inches
-	//and give the correct classification based on that -- should read both as feet or both inches for example
+	//and give the correct classification based on that -- should not read both as feet or both inches for example
 	@Test
-	public void testHeightComboInBMI() {
-		double invalidWt = 500.00 ;
-		fail("Not yet implemented");
+	public void testHeightComboUsedCorrectly1() {
+		int heightFt = 6 ;
+		double heightIn = 2.0 ;
+		//BMI = ((160.7) * 703) / ((74)^2) --> 20.63--> 'HEALTHY'
+		ws = HealthIndicator.calculateWeightStatus(heightFt, heightIn, validWtPds, validAgeYrs) ;
+		assertEquals("if inputs read correctly WeightStatus should be 'HEALTY'", "HEALTHY", ws) ;
 	}
+	
+	@Test
+	public void testHeightComboUsedCorrectly2() {
+		int heightFt = 4 ;
+		double heightIn = 3.0 ;
+		//BMI = ((160.7) * 703) / ((51)^2) --> 43.43--> 'OBESE'
+		ws = HealthIndicator.calculateWeightStatus(heightFt, heightIn, validWtPds, validAgeYrs) ;
+		assertEquals("if inputs read correctly WeightStatus should be 'HEALTY'", "HEALTHY", ws) ;
+	}
+	
+	@Test
+	public void testHeightComboUsedIncorrectly1() {
+		//if both read as feet
+		int heightFt = 5 ;
+		double heightIn = 5.0 ;
+		//10ft --> 120 inches --> (160.7pds*703)/(120in^2) --> 7.85 --> if incorrectly read WS would be 'UNDERWEIGHT'
+		ws = HealthIndicator.calculateWeightStatus(heightFt, heightIn, validWtPds, validAgeYrs) ;
+		assertNotEquals("an incorrect combination interpreting both as feet would be 'UNDERWEIGHT'", "UNDERWEIGHT", ws) ;
+	}
+	
+	@Test
+	public void testHeightComboUsedIncorrectly2() {
+		//if both read as inches
+		int heightFt = 5 ;
+		double heightIn = 11.0 ;
+		//16.0 inches --> (160.7pds*703)/(16in^2) --> 441.29 --> 'OBSESE'
+		ws = HealthIndicator.calculateWeightStatus(heightFt, heightIn, validWtPds, validAgeYrs) ;
+		assertNotEquals("an incorrect combination interpreting both as inches would be 'OBESE'", "OBESE", ws) ;
+	}
+	
+	@Test
+	public void testHeightComboUsedIncorrectly3() {
+		//if it multiplied inches by 12 instead of feet
+		int heightFt = 5 ;
+		double heightIn = 11.0 ;
+		//(11inx12) + 5 = 137.0 inches --> (160.7pds*703)/(137in^2) --> 6.02 --> "UNDERWEIGHT"
+		ws = HealthIndicator.calculateWeightStatus(heightFt, heightIn, validWtPds, validAgeYrs) ;
+		assertNotEquals("an incorrect combination where ht in in. value were mult. by 12 would be 'UNDERWEIGHT'", "UNDERWEIGHT", ws) ;
+	}
+	
+	@Test
+	public void testHeightComboUsedIncorrectly4() {
+		//if both inch and feet values were multiplied by 12
+		int heightFt = 5 ;
+		double heightIn = 11.0 ;
+//		//(11inx12) + 5 = 137.0 inches --> (160.7pds*703)/(137in^2) --> 6.02 --> "UNDERWEIGHT"
+//		ws = HealthIndicator.calculateWeightStatus(heightFt, heightIn, validWtPds, validAgeYrs) ;
+//		assertNotEquals("an incorrect combination where ht in in. value were mult. by 12 would be 'UNDERWEIGHT'", "UNDERWEIGHT", ws) ;
+	}
+	
 
-
+//	BMI = ((weight pds) * 703) / ((height inches)^2)
+//	int validHgt = 5 ;
+//	double validHgtIn = 9.0 ;
+//	double validWtPds = 160.7 ;
+//	int validAgeYrs = 34 ;
 
 	//=============================================================
 	//==============SPEC: Semantically invalid inputs==============
