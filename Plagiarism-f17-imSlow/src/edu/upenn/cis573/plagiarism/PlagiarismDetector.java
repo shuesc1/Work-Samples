771c2ce7123581b61a3004d26d7c30b83a3f485e
Joseph Haymaker <josephhaymaker@Josephs-MacBook-Pro.local>
2017-11-02 20:46:39 -0400

    does analysis of Plagiarism class before refactoring

--- Plagiarism-f17-imSlow/src/edu/upenn/cis573/plagiarism/PlagiarismDetector.java
+++ Plagiarism-f17-imSlow/src/edu/upenn/cis573/plagiarism/PlagiarismDetector.java
@@ -16,116 +16,67 @@ import java.util.Set;
  * (each of which will result in at least a 10% reduction in execution time compared to the
  * original), as well as ten minor inefficiencies.
  */
-
-
 public class PlagiarismDetector {
-		
 	
 	/*
 	 * This method reads the given file and then converts it into a List of Strings.
 	 * It does not include punctuation and converts all words in the file to uppercase.
 	 */
-	private static List<String> readFile(String filename) {
-
-		if (filename == null) return null;
-		
-		List<String> words = new ArrayList<String>();
-		
+	private static List<String> readFile(String filename) { //helper file for below method
+		if (filename == null) {
+			return null;
+		}
+		List<String> words = new ArrayList<String>(); //ArrayList of all words -- allows for repeats
 		try {
-			Scanner in = new Scanner(new File(filename));
-			while (in.hasNext()) {
-				words.add(in.next().replaceAll("[^a-zA-Z]", "").toUpperCase());
+			Scanner in = new Scanner(new File(filename)); //creates new Scanner obj
+			while (in.hasNext()) {	//while more words
+				//TODO should be lowercase -- less changes
+				words.add(in.next().replaceAll("[^a-zA-Z]", "").toUpperCase()); //removes non-letter chars, to upper case
 			}
 		}
 		catch (Exception e) {
 			e.printStackTrace();
 			return null;
 		}
-		
 		return words;
 	}
-
-
 	
 	/*
 	 * This method reads a file and converts it into a Set of distinct phrases,
 	 * each of size "window". The Strings in each phrase are whitespace-separated.
 	 */
 	private static Set<String> createPhrases(String filename, int window) {
-		if (filename == null || window < 1) return null;
-		
-		List<String> words = readFile(filename);
-		
-		Set<String> phrases = new HashSet<String>();
-		
+		if (filename == null || window < 1) {
+			return null;
+		}
+		List<String> words = readFile(filename); //gets ArrayList of all words in doc
+		Set<String> phrases = new HashSet<String>(); //TODO now uses set/hashset - wrong data struct
+		//count up to size -- seems like a list may be a better data struct?
+		//TODO figure out why this counts up to window + 1 -- maybe stop before you have less words
+		// than you need to create a proper phrase
 		for (int i = 0; i < words.size() - window + 1; i++) {
-			String phrase = "";
+			String phrase = ""; //phrase stored as a string -- maybe better as an Array (always fixed size)
+			//
 			for (int j = 0; j < window; j++) {
 				phrase += words.get(i+j) + " ";
+				//TODO phrase = 
 			}
-
-			if (phrases.contains(phrase) == false)
+			if (phrases.contains(phrase) == false) //unnecessary work -- if this is a HM then there can only
+				//be one key instance anyway
 				phrases.add(phrase);
-
-		}
-		
+		}	
 		return phrases;
 	}
 
-	
-
-	/*
-	 * Returns a Map (sorted by the value of the Integer, in non-ascending order) indicating
-	 * the number of matches of phrases of size windowSize or greater between each document in the corpus
-	 * 
-	 * Note that you may NOT remove this method or change its signature or specification!
-	 */
-	public static Map<String, Integer> detectPlagiarism(String dirName, int windowSize, int threshold) {
-		File dirFile = new File(dirName);
-		String[] files = dirFile.list();
-		
-		Map<String, Integer> numberOfMatches = new HashMap<String, Integer>();
-		
-		for (int i = 0; i < files.length; i++) {
-			String file1 = files[i];
-
-			for (int j = 0; j < files.length; j++) { 
-				String file2 = files[j];
-
-				Set<String> file1Phrases = createPhrases(dirName + "/" + file1, windowSize); 
-				Set<String> file2Phrases = createPhrases(dirName + "/" + file2, windowSize); 
-				
-				if (file1Phrases == null || file2Phrases == null)
-					return null;
-				
-				Set<String> matches = findMatches(file1Phrases, file2Phrases);
-				
-				if (matches == null)
-					return null;
-								
-				if (matches.size() > threshold) {
-					String key = file1 + "-" + file2;
-					if (numberOfMatches.containsKey(file2 + "-" + file1) == false && file1.equals(file2) == false) {
-						numberOfMatches.put(key,matches.size());
-					}
-				}				
-			}
-			
-		}		
-		
-		return sortResults(numberOfMatches);
-	}
-	
 	/*
 	 * Returns a Set of Strings that occur in both of the Set parameters.
 	 * However, the comparison is case-insensitive.
 	 */
 	private static Set<String> findMatches(Set<String> myPhrases, Set<String> yourPhrases) {
-	
 		Set<String> matches = new HashSet<String>();
-		
+		//TODO this if statement probs isn't needed
 		if (myPhrases != null && yourPhrases != null) {
-		
+			//TODO super inefficient -- for(all my phrases){if yours.contains(mine), count ++)
 			for (String mine : myPhrases) {
 				for (String yours : yourPhrases) {
 					if (mine.equalsIgnoreCase(yours)) {
@@ -137,22 +88,19 @@ public class PlagiarismDetector {
 		return matches;
 	}
 	
+	//TODO get rid of this method and instead use a TreeMap of (key=match num, value= file names)
 	/*
 	 * Returns a LinkedHashMap in which the elements of the Map parameter
 	 * are sorted according to the value of the Integer, in non-ascending order.
 	 */
 	public static LinkedHashMap<String, Integer> sortResults(Map<String, Integer> possibleMatches) {
-		
 		// Because this approach modifies the Map as a side effect of printing 
 		// the results, it is necessary to make a copy of the original Map
 		Map<String, Integer> copy = new HashMap<String, Integer>();
-
 		for (String key : possibleMatches.keySet()) {
 			copy.put(key, possibleMatches.get(key));
-		}	
-		
+		}		
 		LinkedHashMap<String, Integer> list = new LinkedHashMap<String, Integer>();
-
 		for (int i = 0; i < copy.size(); i++) {
 			int maxValue = 0;
 			String maxKey = null;
@@ -161,18 +109,57 @@ public class PlagiarismDetector {
 					maxValue = copy.get(key);
 					maxKey = key;
 				}
-			}
-			
-			list.put(maxKey, maxValue);
-			
+			}		
+			list.put(maxKey, maxValue);	
 			if (copy.containsKey(maxKey)) {
 				copy.put(maxKey, -1);
 			}
 		}
-		
 		return list;
 	}
 	
+	/*
+	 * Returns a Map (sorted by the value of the Integer, in non-ascending order) indicating
+	 * the number of matches of phrases of size windowSize or greater between each document in the corpus
+	 * 
+	 * Note that you may NOT remove this method or change its signature or specification!
+	 */
+	public static Map<String, Integer> detectPlagiarism(String dirName, int windowSize, int threshold) {
+		File dirFile = new File(dirName);
+		String[] files = dirFile.list(); //list of file names
+		Map<String, Integer> numberOfMatches = new HashMap<String, Integer>();
+		for (int i = 0; i < files.length; i++) { //iterate over all files in corpus
+			String file1 = files[i];
+			for (int j = 0; j < files.length; j++) { 
+				//TODO make sure file isn't compared to itself
+				String file2 = files[j];
+				//uses helper method CREATEPHRASES
+				Set<String> file1Phrases = createPhrases(dirName + "/" + file1, windowSize); 
+				Set<String> file2Phrases = createPhrases(dirName + "/" + file2, windowSize); 
+				if (file1Phrases == null || file2Phrases == null)
+					return null;
+				//uses helper method FINDMATCHES
+				//TODO ESSENTIALLY HELPER METHOD CREATES SET OF MATCHES ONLY TO USE THE SET SIZE
+				//TO DETERMINE IF IT'S ABOVE THE THRESHOLD OR NOT
+				Set<String> matches = findMatches(file1Phrases, file2Phrases);
+				//TODO necessary?
+				if (matches == null)
+					return null;					
+				if (matches.size() > threshold) {
+					String key = file1 + "-" + file2;
+					//TODO unnecessary work
+					if (numberOfMatches.containsKey(file2 + "-" + file1) == false && file1.equals(file2) == false) {
+						numberOfMatches.put(key,matches.size());
+					}
+				}				
+			}
+			
+		}	
+		//uses helper method SORTRESULTS to print out which 
+		return sortResults(numberOfMatches);
+	}
+	
+	//======================= MAIN =============================
 	/*
 	 * This method is here to help you measure the execution time and get the output of the program.
 	 * You do not need to consider it for improving the efficiency of the detectPlagiarism method.
