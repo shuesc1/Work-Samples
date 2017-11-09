ab1a7c4bf5d1ce08bef2075f9ad3edc80e6d5109
Joseph Haymaker <josephhaymaker@Josephs-MacBook-Pro.local>
2017-11-08 19:54:54 -0500

    finishes up with plagiarism class

--- Plagiarism-f17-imSlow/src/edu/upenn/cis573/plagiarism/PlagiarismDetector.java
+++ Plagiarism-f17-imSlow/src/edu/upenn/cis573/plagiarism/PlagiarismDetector.java
@@ -28,42 +28,27 @@ public class PlagiarismDetector {
 		if (filename == null || window < 1) {
 			return null;
 		}
-		//**MAJOR CHANGE** - eliminate the creation of a word list and just use the window from the start to create a phrase list
+//M1: **MAJOR CHANGE** - eliminate the creation of a word list and just use the window from the start to create a phrase list
 		// refactor method so we don't have to change any code below
 		//		List<String> words = readFile(filename); //gets ArrayList of all words in doc
 		Set<String> phrases = new HashSet<String>();
-		//[MINOR CHANGE] - instead of using a giant AL of words just reuse same AL to get (#window) many words, make a phrase, then clear and reuse AL
+//m1: [MINOR CHANGE] - instead of using a giant AL of words just reuse same AL to get (#window) many words, make a phrase, then clear and reuse AL
 		List<String> words = new ArrayList<String>() ;
 		try {
 			Scanner in = new Scanner(new File(filename)); 
-			String thisString = "" ;
-			ArrayList<String> phrase = new ArrayList<String>() ; //need a dynamic data struct for this
-			phrase.add(0, "");
 			while (in.hasNext()) {	
-				//[[[MINOR CHANGE]]] -- to lowercase, not uppercase -- less changes to make
+//m2: [[[MINOR CHANGE]]] -- to lowercase, not uppercase -- less changes to make
 				words.add(in.next().replaceAll("[^a-zA-Z]", "").toLowerCase()); //add each word to words arraylist
-				//				if(words.size() % window == 0) { //then we have the right amount of words stored
-				System.out.println("size of 'words' before: " + words.size()) ;
-				System.out.println("'words' before: " + words.toString()) ;
 				if(words.size() > window-1) {
-//					int startInd = words.size() - window ;
-//					//					String phrase = "" ;
+//m3: [[MINOR CHANGE]] - was using arraylist for the phrase but then just went with using word list
 //					for(int i = 0; i < window; i++) {
 //						phrase.add(words.get(i)) ;
 //					}
 
-//					if (counter < 5) {
-//						System.out.println("Phrase added: " + phrase.toString()) ; //for testing
-//						counter++ ;
-//					}
-//					phrases.add(thisString.toString()) ;
-					System.out.println("'words' to string: " + words.toString()) ;
+					phrases.add(words.toString()) ;
 					words.remove(0) ;
-//					phrase.remove(0) ;
-//					System.out.println("size of 'words' after: " + words.size()) ;
-//					System.out.println("'words' after: " + words.toString()) ;
 
-					//[MINOR CHANGE] -- got rid of check for if phrase already here (see below line)
+//m4: [MINOR CHANGE] -- got rid of check for if phrase already here (see below line)
 					//					phrases.add(phrase) ; // if a repeat it won't be added due to data struct
 					//					words.clear(); //clears up space for next (#window) many words
 				}
@@ -83,15 +68,14 @@ public class PlagiarismDetector {
 	 */
 	static int findMatches(Set<String> myPhrases, Set<String> yourPhrases ) {
 		int matches = 0 ;
-		//[MINOR CHANGE] - make a copy of your phrases so each matching phrase can be taken out in an attempt to speed up run time
+//m5: [MINOR CHANGE] - make a copy of your phrases so each matching phrase can be taken out in an attempt to speed up run time
 		// (for files with large number of matches)
 		Set<String> yourPhrasesCopy = yourPhrases ;
-		/* **MAJOR CHANGE** - got rid of nested for loop, now just iterating over my phrase list */
+//M2: ***MAJOR CHANGE** - got rid of nested for loop, now just iterating over my phrase list */
 		for (String mine : myPhrases) {
 			if(yourPhrasesCopy.contains(mine)) {
-				System.out.println("Match: " + mine) ;
-				matches++; //[MINOR CHANGE] - changed return type to int, so now not wasting space
-				System.out.println("no. matches: " + matches) ;
+//m6: [MINOR CHANGE] - changed return type to int, so now not wasting space				
+				matches++; 
 				yourPhrasesCopy.remove(mine) ;
 			}
 		}	
@@ -112,24 +96,25 @@ public class PlagiarismDetector {
 		ArrayList<String> processedPairs = new ArrayList<String>() ;
 		for (int i = 0; i < files.length; i++) { //iterate over all files in corpus
 			String file1 = files[i]; 
-			Set<String> file1Phrases = createPhrases(dirName + "/" + file1, windowSize); //***MAJOR CHANGE*** - move this to outer for loop so it's only done once, not j many times
+//M3: ***MAJOR CHANGE*** - move this to outer for loop so it's only done once, not j many times			
+			Set<String> file1Phrases = createPhrases(dirName + "/" + file1, windowSize); 
 			for (int j = 0; j < files.length; j++) { 
 				String file2 = files[j]; 
-				//[[MINOR CHANGE]] - moved the 2 identical file comparison names out here so the same pairs of files aren't checked twice
+//m7: [[MINOR CHANGE]] - moved the 2 identical file comparison names out here so the same pairs of files aren't checked twice
 				String key1 = file1 + "-" + file2;
 				String key2 = file2 + "-" + file1;
-				if(!file1.equals(file2) && !processedPairs.contains(key1) && !processedPairs.contains(key2)) { /*[[MINOR CHANGE]]] -- don't compare file to itself */
+//M4: [[MAJOR CHANGE]]] -- don't compare file to itself */				
+				if(!file1.equals(file2) && !processedPairs.contains(key1) && !processedPairs.contains(key2)) { 
 					Set<String> file2Phrases = createPhrases(dirName + "/" + file2, windowSize); 
 					if (file1Phrases == null || file2Phrases == null)
 						return null;
-					System.out.println("Files compared: " + key1) ;
-					int matches = findMatches(file1Phrases, file2Phrases);
-					//			if (matches == null){ return null;	} /* [[MINOR CHANGE]] - unnecessary, removing statement					
+					int matches = findMatches(file1Phrases, file2Phrases) ;
+//m8: [[MINOR CHANGE]] - unnecessary, removing statement					
+					//			if (matches == null){ return null;	} 					
 					if (matches > threshold) {
-						//[[MINOR CHANGE]] -- removed " && file1.equals(file2) == false" condition from below since same files shouldn't ever be compared
+//m9: [[MINOR CHANGE]] -- removed " && file1.equals(file2) == false" condition from below since same files shouldn't ever be compared
 						if (numberOfMatches.containsValue(key1) == false) {
 							numberOfMatches.put(key1, matches);
-							System.out.println("key: " + key1 + "match no.: " + matches) ;
 							processedPairs.add(key1);
 							processedPairs.add(key2);
 						}
@@ -139,7 +124,7 @@ public class PlagiarismDetector {
 			}
 
 		}
-		//[[MINOR CHANGE]]- tried using treemap for descending sorting, but then realized that I'd need repeat keys (not possible w/ TM)
+//m10: [[MINOR CHANGE]]- tried using treemap for descending sorting, but then realized that I'd need repeat keys (not possible w/ TM)
 		//instead created a (hopefully) more efficient sorting method
 		return MapSort.sortByValue(numberOfMatches) ;
 	}
@@ -167,11 +152,19 @@ public class PlagiarismDetector {
 			System.out.println(entry.getKey() + ": " + entry.getValue());
 		}
 
+		// ======= times for 5 trials -- original version =========/
 		// t1: 115.358 seconds
 		//t2: 124.048 seconds
 		//t3: 114.542 seconds
 		//t4: 114.883 seconds
 		//t5: 129.186 seconds
+		
+		// ======= times for 5 trials -- refactored version =========/
+		//t1: 7.291 seconds 
+		//t2: 6.714 seconds 
+		//t3: 6.731 seconds 
+		//t4: 6.845 seconds 
+		//t5: 6.65 seconds 
 	}
 
 }
