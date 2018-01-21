498e3068a52b893706e36ca52f899eda14309b10
Joseph Haymaker <josephhaymaker@Josephs-MacBook-Pro.local>
2018-01-20 22:09:41 -0500

    more c proj work

--- CIT595_HW1/practice.c
+++ CIT595_HW1/practice.c
@@ -15,6 +15,7 @@ int main() {
 	char str2[] = "int fun1 ( int a ) {" ;
 	char str3[] = "int fun2 ( int dog , int cat ) {" ;
 	char str4[] = "int fun3 ( int larry , int moe , int curly ) {" ;
+	char str5[] = "int fun3 ( int param1 , int param2 , int param3 , int param4 ) {" ;
 
 	all_str[0] = str[0];
 	all_str[1] = str1[0];
@@ -34,8 +35,8 @@ int main() {
 //=======================================================
 	// printf ("Splitting string \"%s\" into tokens:\n", str1);
 	// token = strtok (str1, " ");
-	printf ("Splitting string \"%s\" into tokens:\n", str2);
-	token = strtok (str2, " ");
+	printf ("Splitting string \"%s\" into tokens:\n", str5);
+	token = strtok (str5, " ");
 	// printf ("Splitting string \"%s\" into tokens:\n", str3);
 	// token = strtok (str3, " ");
 	char* toks[10];
@@ -43,29 +44,74 @@ int main() {
 	toks[x] = token;
 	x++;
 	while (token != NULL) {
-		printf ("%s\n", token);
+		// printf ("%s\n", token);
 		token = strtok (NULL, " ");
+		printf ("%s\n", token);
 		if(token == NULL){
 			break;
 		}
 		toks[x] = token ;
+		printf("toks[%d]: %s\n", x, toks[x]);
 		x++;
 	}
 	toks[x] = "\0" ;
 
+	// printf("%s\n", toks[0]);
+	// printf("%x\n", &toks[0]);
+	// printf("%c\n", *toks[0]);
+	// printf("%s\n", toks[1]);
+	// printf("%s\n", toks[2]);
+	// printf("%s\n", toks[3]);
+	// printf("%s\n", toks[4]);
+	// printf("%s\n", toks[5]);
+	// printf("%s\n", toks[6]);
+
+
+	// ------------ TOKEN ARRAY -------------
 	printf("contents of token array: \n") ;
 	//char * current_tok;
 	int length = x;
 	// int length = strlen(toks);
 	printf("length: %d\n", length);
 	for(int q = 0; q < length; q++){
-		printf("%s\n", toks[q]);
+		printf("toks[%d]: %s\n", q, toks[q]);
 		//printf("%c\n", *toks[q]); //that specific char
 		// printf("%s\n", &toks[q]); //ptr to ptr
 	}
 
+
+
+	// ------------- getting parameters ---------
+	// if(length == 5){
+	// 	continue;
+	// } else if(length > 5){
+	if(length > 5){
+		//toks[2] will always be '\(' && toks[length - 2] will always be '\)'
+		// toks[4] will be the 1st param is there is one, and toks[length - 3] will be the last
+		// params (if multiple) will be 3 indices apart
+		int a = 4;
+		int b = 0;
+		int limit = length-2;
+		// for(int a = 0; a < (length-2); a=a+3){
+		while(a < limit){
+			parameter_names[b] = toks[a];
+			a+=3;
+			b++;
+		}
+	}
+
+	printf("======== OUTPUT: ==========") ;
 	function_name = toks[1] ;
 	printf("\nfunction name: %s\n", function_name);
+
+	for(int v = 0; v < 11; v++){
+		printf("parameter_names[%d]: %s\n", v, parameter_names[v]);
+	}
+
+
+
+
+
 
 //======================================
 	// printf ("Splitting string \"%s\" into tokens:\n",str3);
