f55fe290a1f02b4998cba78afacd34365383bba6
Joseph Haymaker <josepma@seas.upenn.edu>
2018-02-16 10:59:09 -0500

    fixes offset error

--- CIT595_HW2/symbol_table.c
+++ CIT595_HW2/symbol_table.c
@@ -7,7 +7,7 @@
 typedef struct Node node ;
 struct Node {
 	void* symbol;
-	int* offset;
+	int offset;
 	node* next;
 	node* prev;
 };
@@ -30,6 +30,8 @@ extern int parse_line(char*);
 extern char* function_name;
 extern char* parameter_names[10];
 extern char* variable_names[10];
+
+// foo->bar == (*foo).bar
 
 /*
  * THIS IS THE STARTER CODE FOR PART 1 
@@ -72,11 +74,13 @@ int contains(node* ht[], char* target){ // 1 - found; 0 - not found
  * A helper function for the get_offset primary function that traverses linked nodes in a hashtable
  */
 int get_existing_offset(node* ht[], char* sym){ // non-zero - found; 0 - not found
+	//printf("running get_existing_offset() searching for %s\n",sym);
 	int hashcode = hash_func(sym, SIZE); // get index
 	node* head = ht[hashcode]; // head is at certain index in hash array; may contain several linked nodes
 	while(head!=NULL){
 		if(strcmp(head->symbol, sym)==0){
-			return *head->offset;
+			//printf("symbol found: head->offset: %d\n",head->offset);
+			return head->offset;
 		} else {
 			head = head->next;
 		}
@@ -100,8 +104,11 @@ int add(node* ht[], char* sym, int offst){ // 0 - error ; 1 - added correctly
 
 	strcpy(new->symbol, sym);
 	//strcpy(new->offset, offst);
-	new->offset = &offst;
-
+	//new->offset = &offst;
+	new->offset = offst;
+	//sprintf(new->offset, "%p", &offst); 
+	//printf("new->offset: %d\n", new->offset);
+        //printf("*new->offset: %d\n", *new->offset);
 	new->next = head; // new now points to head
 	new->prev = NULL; // new points back to NULL
 	if(head!=NULL){
@@ -167,10 +174,10 @@ void clear() {
 		if(n1==NULL){
 			printf("[%d]|NULL|\n", k);
 		} else {
-			printf("[%d]|symbol: %s, offset: %d|", k, n1->symbol, *n1->offset);
+			printf("[%d]|symbol: %s, offset: %d|", k, n1->symbol, n1->offset);
 			//int l = k;
 			while(n1->next != NULL){
-				printf(" ===> |symbol: %s, offset: %d| ", n1->next->symbol, *n1->next->offset);
+				printf(" ===> |symbol: %s, offset: %d| ", n1->next->symbol, n1->next->offset);
 				n1 = n1->next;
 			}
 			printf("\n");
@@ -185,7 +192,7 @@ void clear() {
 			// in case there’s only one node
 			// space malloc'd for node, node->symbol, & node->offset
 			if (n->next == NULL) {
-				printf("freeing node at %p with [symbol] %s and [offset] %d\n", n, n->symbol, *n->offset);
+				printf("freeing node at %p with [symbol] %s and [offset] %d\n", n, n->symbol, n->offset);
 				free(n->symbol);
 				n->symbol = NULL; // free symbol and set to NULL
 				//free(n->offset);
@@ -215,7 +222,7 @@ void clear() {
 				}
 				// broke out -- means n->prev == NULL (at head)
 				if(n->prev == NULL && n->next!= NULL){
-					printf("freeing node at %p with [symbol] %s and [offset] %d\n", n, n->next->symbol, *n->next->offset);
+					printf("freeing node at %p with [symbol] %s and [offset] %d\n", n, n->next->symbol, n->next->offset);
 					// 1) free next's symbol
 					free(n->next->symbol);
 					n->next->symbol = NULL;
@@ -227,7 +234,7 @@ void clear() {
 					n->next = NULL;   	
 				} 
 				if(n->prev == NULL && n->next == NULL){
-					printf("freeing node at %p with [symbol] %s and [offset] %d\n", n, n->symbol, *n->offset);
+					printf("freeing node at %p with [symbol] %s and [offset] %d\n", n, n->symbol, n->offset);
 					free(n->symbol);
 					n->symbol = NULL;
 					//free(n->offset);
@@ -458,10 +465,8 @@ void clear_add_indices(){
  * for if this is the 1st iteration or not (0-yes, 1-No)
  */
 int generate_asm_addition(int tok_index_L, int tok_index_R, char* register_L_var, int first_iter_flag, FILE* dest_file){
-	char* left_tok = malloc(sizeof(char)*strlen(arr_char_arrs[tok_index_L]));
-	left_tok = arr_char_arrs[tok_index_L];
-	char* right_tok = malloc(sizeof(char)*strlen(arr_char_arrs[tok_index_R]));
-	right_tok = arr_char_arrs[tok_index_R]; 
+	char* left_tok = arr_char_arrs[tok_index_L];
+	char* right_tok = arr_char_arrs[tok_index_R]; 
 	char* asm_line1 = malloc(sizeof(char)*255);
 	char* asm_line2 = malloc(sizeof(char)*255);
 
@@ -472,7 +477,7 @@ int generate_asm_addition(int tok_index_L, int tok_index_R, char* register_L_var
 			//snprintf(prefix, sizeof(prefix), "%s: %s: %s", argv[0], cmd_argv[0], cmd_argv[1]);
 			//snprintf(asm_line1, sizeof(asm_line1), "%s%s, %s, #%d\n", oper[3], regs[0], regs[0], 0);// 'AND R0, R0, #0
 			snprintf(asm_line1, 100, "AND R0, R0, #%d\n", 0);// 'AND R0, R0, #0
-			printf("asm_line1: %s", asm_line1);
+			//printf("asm_line1: %s", asm_line1);
 			fputs(asm_line1, dest_file);
 			strcpy(asm_line1, ""); // clear line
 			snprintf(asm_line1, 100, "ADD R0, R0, #%s\n",right_tok);// 'ADD R0, R0, #RIGHT_TOKEN_NUM'
@@ -481,7 +486,7 @@ int generate_asm_addition(int tok_index_L, int tok_index_R, char* register_L_var
 		} else if(is_number(right_tok)!=0){ // !0-9
 			snprintf(asm_line1, 100, "LDR R0, FP, #%d\n", get_existing_offset(hashtable, right_tok));// 'LDR R0, FP, #offset_sym
 			printf("right tok: %s, offset: %d\n", left_tok, get_existing_offset(hashtable, right_tok));
-			printf("asm_line1: %s", asm_line1);
+			//printf("asm_line1: %s", asm_line1);
 			fputs(asm_line1, dest_file);
 			strcpy(asm_line1, ""); // clear line
 		}
@@ -491,7 +496,7 @@ int generate_asm_addition(int tok_index_L, int tok_index_R, char* register_L_var
 	// ------------- 1st or 2nd++ iteration --------- only L side loaded   
 	if(is_number(left_tok)==0){ // is 0-9
 		snprintf(asm_line2, 100, "AND %s, %s, #0\n", register_L_var, register_L_var);
-		printf("asm_line2: %s", asm_line2);
+		//printf("asm_line2: %s", asm_line2);
 		fputs(asm_line2, dest_file);
 		strcpy(asm_line2, "");
 		snprintf(asm_line2, 100, "ADD %s, %s, #%s", register_L_var, register_L_var, left_tok);
@@ -500,7 +505,7 @@ int generate_asm_addition(int tok_index_L, int tok_index_R, char* register_L_var
 	} else if(is_number(left_tok)!=0){ // !0-9
 		snprintf(asm_line2, 100, "LDR %s, FP, #%d\n", register_L_var, get_existing_offset(hashtable, left_tok));// 'LDR R#, FP, #offset_sym
 		printf("left tok: %s, offset: %d\n", left_tok, get_existing_offset(hashtable, left_tok));
-		printf("asm_line2: %s", asm_line2);
+		//printf("asm_line2: %s", asm_line2);
 		fputs(asm_line2, dest_file);
 		strcpy(asm_line2, ""); // clear line
 	}
@@ -512,9 +517,7 @@ int generate_asm_addition(int tok_index_L, int tok_index_R, char* register_L_var
 	strcpy(asm_line2, ""); // clear line
 
 	// freeing vars
-	//free(left_tok);
 	left_tok = NULL;
-	//free(right_tok);
 	right_tok = NULL;
 	free(asm_line1);
 	asm_line1 = NULL;
@@ -530,18 +533,18 @@ int generate_asm_assignment(int tok_index_L, int tok_index_R, int return_flag, F
 	// int x = y OR t = 5 OR t = q + u OR return g OR return y + t +v
 	char* asm_line1 = malloc(sizeof(char)*strlen(arr_char_arrs[tok_index_L]));
 	char* asm_line2 = malloc(sizeof(char)*strlen(arr_char_arrs[tok_index_R]));
-	char* left_tok = malloc(sizeof(char)*strlen(arr_char_arrs[tok_index_L]));
-	left_tok = arr_char_arrs[tok_index_L];
-	char* right_tok = malloc(sizeof(char)*strlen(arr_char_arrs[tok_index_R]));
-	right_tok = arr_char_arrs[tok_index_R];
+	//char* left_tok = malloc(sizeof(char)*strlen(arr_char_arrs[tok_index_L]));
+	char* left_tok = arr_char_arrs[tok_index_L];
+	//char* right_tok = malloc(sizeof(char)*strlen(arr_char_arrs[tok_index_R]));
+	char* right_tok = arr_char_arrs[tok_index_R];
 	if(return_flag==1){
 		snprintf(asm_line1, 100, "STR R0, FP, #%d\n", 3);
-		printf("assignment ASM: %s", asm_line1);
+		//printf("assignment ASM: %s", asm_line1);
 		fputs(asm_line1, dest_file);
 		strcpy(asm_line1, "");
 	} else if(return_flag!=0 && tok_index_R==0){ // int x = .. OR return y (right side already in R0 hypothetically)
 		snprintf(asm_line1, 100, "STR R0, FP, #%d\n", get_existing_offset(hashtable, left_tok));
-		printf("assignment ASM:: %s", asm_line1);
+		//printf("assignment ASM:: %s", asm_line1);
 		fputs(asm_line1, dest_file);
 		strcpy(asm_line1, "");
 	} else {
@@ -568,7 +571,9 @@ int generate_asm_assignment(int tok_index_L, int tok_index_R, int return_flag, F
 
 /*
  * The major function that takes in a current buffer line and generates asm depending on its properties
- */
+* break line in to tokens and store tokens in array-- then use token attributes to call 1.) delcaration(), 2.) one_assignment(), 3.) multiple assignments(), 4.)return()
+* if line has multiple assignments or commas is split on commas into smaller sublines and then processed accordingly
+*/
 int process_line(char* buffer, FILE* file_output){
 	int num_assignments = 0;// necessary strings & values
 	int num_additions = 0;
@@ -630,7 +635,6 @@ int process_line(char* buffer, FILE* file_output){
 		// ======================= 3.) (possible) declaration and 2 or more assignments ===================
 	} else if(num_assignments>1 || num_commas>0){ // 3.) (possible) declaration and 2 or more assignments
 		printf("line is type 3). multiple assignments '=' in line\n");
-
 		//break string into smaller tokens on the ',' and store in global arr multiple_assign_subarr[10];
 		int x = 0;
 		//char* copy = malloc(sizeof(char)*strlen(buffer)+1);
@@ -681,7 +685,7 @@ int process_line(char* buffer, FILE* file_output){
 // -------------- helper funcs (above) ------------------
 // ------------------------------------------------------
 
-
+// TODO do part 2, 2nd part -- error handling
 //TODO global-- free all mallocs
 
 /*
@@ -723,6 +727,7 @@ int generate_asm(char* orig_filename, char* lc4_filename){
 			continue;
 		} else { //no longer at header
 			// break line in to tokens and store tokens in array-- then use token attributes to call delcaration(), one_assignment(), multiple assignments(), return()
+			// all above encapsulated in below function
 			process_line(buffer, file_output);
 		} 
 		clear_add_indices();// wipe this after each line
@@ -732,7 +737,6 @@ int generate_asm(char* orig_filename, char* lc4_filename){
 
 	return 0;
 }
-
 
 
 
