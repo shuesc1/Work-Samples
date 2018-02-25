36d1fb812867aac8478b1c3d3810ba7786d0d8f4
Joseph Haymaker <josepma@seas.upenn.edu>
2018-02-24 21:36:21 -0500

    adds concurrency C part

--- CIT595_HW3/numbers.c
+++ CIT595_HW3/numbers.c
@@ -3,95 +3,95 @@
 #include <unistd.h>
 #include <pthread.h>
 #include <ctype.h>
+#include <string.h>
 
 void* fun1(void*);
 void* fun2(void*);
 float input_vals[100]; // global float array to hold all input values
 int index_arr; // index of current location in array
-int quit, max, min, total;
+int quit, max, min, total, current_ind;
+
 
 /*
 // dummy func
 int sq(int num){
-	int ret = num * num;
-	return ret;
+int ret = num * num;
+return ret;
 }
+ */
 
 void* fun1(void* p){
-	int k = *(int*)p; // int value at int ptr p
-	int* r = malloc(sizeof(int));
-	if(r==NULL) printf("error3: malloc error\n") ;
+	char in[100];
+	float val_input;
 
-	//*r = do_something(k);
-	*r = sq(k);
-	printf("fun1 returning value: %d\n", *r);
-	pthread_exit(r);
-}
-*/
-
-
-void* fun1(void* p){
-  char in[100];
-  float val_input;
-  //while(q != 1){
-	printf("Please input a valid floating point number: \n");
-	fgets(in, 100, stdin);
-	val_input = atof(in);
-	if(strcmp(in, "q")==0 || strcmp(in,"Q")==0){
-		quit = 1;
-		continue;
-	} else if(val_input != 0.0){ //input is valid
-		input_vals[index_arr] = val_input;
-		index_arr++;
-	} else { // conversion couldn't happen (chars) OR 0.0 entered
-
+	while(quit != 1){
+		printf("Please input a valid floating point number: \n");
+		fgets(in, 100, stdin);
+		if(strcmp(in,"q\n")==0 || strcmp(in,"Q\n")==0){
+			quit = 1;
+			printf("quit entered\n");
+			break;
+		}
+		val_input = atof(in);
+		if(val_input != 0.0){ //input is valid float/double
+			input_vals[index_arr] = val_input;
+			index_arr++; // increase index of global array -- should only be modified here
+		} else { // conversion couldn't happen (chars) OR 0.0 entered
+			printf("Input not a valid float!!\n");
+			//TODO account for if input is 0/0.0/0.0000000
+		}
 	}
-
-  //}
+	void* v = NULL;
+	//*(int*)v = NULL;
+	pthread_exit(v);
 }
-
-/*
-   void* fun2(void* p){
-   int k = *(int*)p; // int value at int ptr p
-   int* r = malloc(sizeof(int));
-   if(r==NULL) printf("error3: malloc error\n") ;
-
- *r = do_something(k);
- *r = sq(k);
- printf("fun2 returning value: %d\n", *r);
- pthread_exit(r);
- } */
 
 void* fun2(void* p){
 	int avg = 0;
 	do{
 		sleep(10);
-		if(p!=NULL){
-			int new_entry = *(int*)p;
-			if(new_entry > max){
-				max = new_entry;
+		if(current_ind < index_arr){ // a new value will have been added -- if not no need to recompute all values
+			//int new_entry = *(int*)p;
+			//int new_entry = input_vals[index_arr];
+			//printf("new_entry: %d\n", new_entry);
+			//printf("current max: %d\n", max);
+			int new_entry = 0;
+			int total = 0;
+			for(int i=0; i<index_arr; i++){
+				new_entry = input_vals[i];
+				total+=input_vals[i];
+				if(new_entry > max){
+					max = new_entry;
+					printf("new max set to %d\n", max);
+				}
+				if(new_entry < min){
+					min = new_entry;
+					printf("new min set to %d\n", min);
+				}
 			}
-			if(new_entry < min){
-				min = new_entry;
-			}
+			//new_entry = input_vals[index_arr];
+			avg = (total)/(index_arr);	
+			current_ind = index_arr;
 		}
-		avg = (total+new_entry)/(index_arr+1)
 		printf("maximum value input so far: %d\n", max);
 		printf("minimum value input so far: %d\n", min);
 		printf("average of all values input so far: %d\n", avg);
 		int floor = index_arr - 5;
 		if(floor<0){ floor=0; }
 		printf("last 5 values input:\n");
-		for(int ctr=index_arr; ctr>=floor; ctr--){
-			printf("%d\n", input_vals[ctr]);
+		for(int ctr=index_arr-1; ctr>=floor; ctr--){
+			printf("%f\n", input_vals[ctr]);
 		}
-	
-		int* r = malloc(sizeof(int));
-		if(r==NULL) printf("error3: malloc error\n") ;
-		r = 0;
+
+		//int* r = malloc(sizeof(int));
+		//if(r==NULL) printf("error3: malloc error\n") ;
+		//r = 0;
 	} while(quit != 1);
 
-	pthread_exit(r);
+	void* v = NULL;
+	//*(int*)v = 1;
+
+	pthread_exit(v);
 }
 
 /*
@@ -102,6 +102,7 @@ int main(){
 	quit = 0;
 	max = 0;
 	min = 0;
+	current_ind = 0;
 	int ret_val;
 	pthread_t t1, t2 ;
 	int a1 = 2, a2 = 4; 
