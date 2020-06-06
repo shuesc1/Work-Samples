1106553547f9ccfe0d2d0049d855f98a6aa15b9a
Joseph Haymaker <josepma@seas.upenn.edu>
2020-06-06 16:52:05 -0700

    Starts changing file structure.

---
+++ programming/java/JUnitTest/src/test/TestJunit.java
@@ -0,0 +1,14 @@
+package test;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestJunit {
+	   
+	@Test
+	   public void testSetup() {
+	      String str= "I am done with Junit setup";
+	      assertEquals("I am done with Junit setup",str);
+	   }
+	
+}
